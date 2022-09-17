3e3671636c45728fab6865a6425f79ac1d8533f7
hamza <hamza.rzine@student.vinci.be>
2022-09-17 16:04:50 +0200

    commit 1.2

--- exercices/module1/1.2/index.js
+++ exercices/module1/1.2/index.js
@@ -1,9 +1,9 @@
 const dateTimeNow=new Date()
 let message="This is the best moment to have a look at this website !"
 
+addDateTime(message)
 console.log(dateTimeNow.toLocaleDateString());
 
-addDateTime(message)
 
 function  addDateTime (message) {
     alert(dateTimeNow.toLocaleDateString () + " : " +message)
