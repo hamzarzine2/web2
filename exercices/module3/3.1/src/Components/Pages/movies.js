562803f9935c03275c7890fd5b26c46fdc0d3af4
hamza <hamza.rzine@student.vinci.be>
2022-10-03 17:52:50 +0200

    myMovies : module 3 : refactoring & MooviePage

--- exercices/module3/3.1/src/Components/Pages/movies.js
+++ exercices/module3/3.1/src/Components/Pages/movies.js
@@ -3,7 +3,7 @@ film.push({
     title : 'one piece : red',
     duration : 290,
     budget : 230,
-    link : 'https://amazing-javascript.world'
+    link : 'https://www.allocine.fr/film/fichefilm_gen_cfilm=298674.html'
 })
 
 function addfilm (titre,duree,argent,lien){
@@ -19,7 +19,7 @@ function filmAsLine(){
     let ligne=""
     film.forEach((item) => {
         ligne+='<tr>'
-        ligne+=`<td><a href="">${item.title}</a></td>`
+        ligne+=`<td><a href=${item.link}>${item.title}</a></td>`
         ligne+=`<td>${item.duration}</td>`
         ligne+=`<td>${item.budget}</td>`
         ligne+='</tr>'
