edf5eddd85b2198b72959b8c93fe9b41d6d3a0c9
hamzarzine2 <hamza.rzine@student.vinci.be>
2022-10-10 10:21:45 +0200

    myMovies : module 4 : basic API

--- exercices/module4/4.1/myMovies/routes/films.js
+++ exercices/module4/4.1/myMovies/routes/films.js
@@ -41,8 +41,7 @@ let film =[{
 
     router.get('/',(req,res,next) => {
         const filterDuration= req?.query?.['minimum-duration'];
-        console.log(typeof filterDuration)
-        if(typeof filterDuration === 'number'){
+        if(filterDuration === undefined){
             res.json(film);
             return;
         }
@@ -59,6 +58,60 @@ let film =[{
         res.json(film[filmID]);
 });
 
+
+    router.post('/',(req,res) => {
+        const title= req?.body?.title.length !== 0 ? req.body.title : undefined;
+        const duration=req?.body?.duration.length !== 0 ? req.body.duration : undefined;
+        const budget=req?.body?.budget.length !== 0 ? req.body.budget : undefined;
+        const link = req?.body?.link.length !== 0 ? req.body.link : undefined;
+
+        if(!title || !duration || !budget || !link) return res.sendStatus(400);
+      
+        const idAdd=film.length+1;
+
+        const itemAdd={
+                id : idAdd,
+                title: title,
+                duration: duration,
+                budget: budget,
+                link: link
+        }
+
+        film.push(itemAdd);
+
+        res.json(itemAdd);
+});
+
+    router.delete('/:id', (req,res) => {        
+        console.log(req.params.id)
+        if (req.params.id<0 || req.params.id>film.length) return res.sendStatus(404);
+        const filmID=[...film].findIndex(e => e.id == req.params.id);
+        const filmSupp=film[filmID];
+        film.splice(filmID,1);
+        res.json(filmSupp);
+        
+    });
+
+        router.patch('/:id',(req,res) => {
+            
+            const titre=req?.body?.title;
+            const duree=req?.body?.duration;
+            const budget=req?.body?.budget;
+            const lien=req?.body?.link;
+
+            
+            if (req.params.id<0 || req.params.id > film.length) return res.sendStatus(404);
+
+            if((!titre && !duree && !budget && !lien) || titre?.length===0 
+            || duree?.length===0||budget?.length===0||lien?.length===0)return res.sendStatus(400);
+            const filmID=[...film].findIndex(e => e.id == req.params.id);
+   
+            const filmUptad={...film[filmID],...req.body}
+
+            film[filmID]=filmUptad;
+
+            res.json(filmUptad);
+        });
 
 
 
